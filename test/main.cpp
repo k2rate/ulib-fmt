d064446cb71cb53312b55a9a2adcf5b316d23215
k2re8 <dns.1983n@gmail.com>
2023-02-15 10:24:48 +0300

    ulib::format

--- test/main.cpp
+++ test/main.cpp
@@ -1,40 +1,56 @@
-#include <ulib/encodings/utf16/string.h>
-#include <ulib/encodings/utf32/string.h>
-
 #include <ulib/string.h>
 #include <ulib/wchar.h>
 #include <ulib/u8.h>
 #include <ulib/u16.h>
 #include <ulib/u32.h>
 
-#include <ulib/fmt/format.h>
-#include <ulib/fmt/path.h>
+#include <ulib/format.h>
 
 #include <windows.h>
 
 int main()
 {
+    bool bchar = false;
+#ifdef __cpp_char8_t
+    bchar = true;
+#endif
+
     ulib::string_view ky;
     ulib::u8string_view re;
 
     ulib::u8string u8str(u8"pizdec");
     ulib::u16string u16str(u"ахуеть");
     ulib::u32string u32str(U"ЕБАТЬ");
-
     ulib::u8string_view test = u8str;
 
-    ulib::format<ulib::Utf8>(u8"{}", u8str);
-    ulib::format(u8"hi {}", u16str);
-
-    bool bchar = false;
-#ifdef __cpp_char8_t
-    bchar = true;
-#endif
-
     ulib::u8string u8out = ulib::format(u8"bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
-                                        bchar, u8str, u16str, u32str, test, std::filesystem::current_path());
+                                        bchar, u8str, u16str, u32str, test,
+                                        std::filesystem::current_path());
 
-    auto u16out = ulib::Convert<ulib::Utf8, ulib::Utf16>(u8out);
+    ulib::u16string u16out = ulib::format(u"bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
+                                          bchar, u8str, u16str, u32str, test,
+                                          std::filesystem::current_path());
+
+    ulib::u32string u32out = ulib::format(U"bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
+                                          bchar, u8str, u16str, u32str, test,
+                                          std::filesystem::current_path());
+
+    ulib::string out = ulib::format("bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
+                                    bchar, u8str, u16str, u32str, test,
+                                    std::filesystem::current_path());
+
+    ulib::wstring wout = ulib::format(L"bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
+                                      bchar, u8str, u16str, u32str, test,
+                                      std::filesystem::current_path());
+
+    u32out = ulib::format<ulib::Utf32>(u8"bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
+                                       bchar, u8str, u16str, u32str, test,
+                                       std::filesystem::current_path());
+
+    wout = ulib::format<ulib::WideEncoding>(u8"bchar: {} u8:{} u16:{} u32:{} u8string_view:{}\ncurrent_path: {}",
+                                            bchar, u8str, u16str, u32str, test,
+                                            std::filesystem::current_path());
+
     MessageBoxW(0, (wchar_t *)u16out.c_str(), L"ky", MB_OK);
 
     return 0;
--- ulib-fmt/ulib/fmt/format.h
+++ ulib-fmt/ulib/fmt/format.h
@@ -4,10 +4,72 @@
 #include <ulib/containers/stringview.h>
 
 #include <ulib/u8.h>
+#include <ulib/string.h>
 
 #include <ulib/allocators/standardallocator.h>
+#include <ulib/allocators/mallocallocator.h>
+#include <ulib/allocators/fastmemallocator.h>
+#include <ulib/allocators/staticallocator.h>
+#include <ulib/allocators/synchronizedallocator.h>
 
 #include <fmt/format.h>
+
+namespace ulib
+{
+    namespace detail
+    {
+        using FormatAllocatorT = ulib::StaticAllocator<ulib::SynchronizedAllocator<ulib::FastMemAllocator<ulib::MallocAllocator>>>;
+
+        template <class EncodingT>
+        constexpr bool IsRawEncoding = EncodingT::kType == EncodingType::Raw;
+
+        template <class EncodingT, class CharT>
+        constexpr bool IsEncodingSizeCompatible = sizeof(typename EncodingT::CharT) == sizeof(CharT);
+
+        template <class EncodingT, class CharT>
+        constexpr bool IsRawEncodingLike = IsRawEncoding<EncodingT> && IsEncodingSizeCompatible<EncodingT, CharT>;
+
+        template <class EncodingT>
+        constexpr bool IsEncodingFmtCompatible = std::is_same_v<EncodingT, Utf8> || detail::IsRawEncodingLike<EncodingT, char>;
+
+        template <class T, class DefaultT>
+        struct SelectType
+        {
+            using Type = T;
+        };
+
+        template <class DefaultT>
+        struct SelectType<void, DefaultT>
+        {
+            using Type = DefaultT;
+        };
+
+        template <class T, class DefaultT>
+        using SelectTypeT = typename SelectType<T, DefaultT>::Type;
+
+        template <typename FormatContext, class EncodingT>
+        auto FormatHandler(const ulib::EncodedStringView<EncodingT> p, FormatContext &ctx)
+        {
+            fmt::formatter<fmt::string_view> ft;
+
+            if constexpr (IsEncodingFmtCompatible<EncodingT>)
+            {
+                fmt::string_view name((char *)p.Data(), p.Size());
+                return ft.format(name, ctx);
+            }
+            else
+            {
+                auto u8str = Convert<Utf8, FormatAllocatorT>(p);
+
+                fmt::string_view name((char *)u8str.Data(), u8str.Size());
+                return ft.format(name, ctx);
+            }
+        }
+
+        // template <class SourceEncodingT, class EncodingT>
+        // inline constexpr bool IsParentCompatible = std::is_same_v<SourceEncodingT, typename EncodingT::ParentEncodingT> || std::is_same_v<typename SourceEncodingT::ParentEncodingT, EncodingT>;
+    }
+}
 
 template <class EncodingT, class AllocatorT>
 struct fmt::formatter<ulib::EncodedString<EncodingT, AllocatorT>>
@@ -26,21 +88,7 @@ struct fmt::formatter<ulib::EncodedString<EncodingT, AllocatorT>>
     template <typename FormatContext>
     auto format(const StringT &p, FormatContext &ctx)
     {
-        if constexpr (std::is_same_v<EncodingT, ulib::Utf8>)
-        {
-            fmt::string_view name((char *)p.Data(), p.Size());
-
-            fmt::formatter<fmt::string_view> ft;
-            return ft.format(name, ctx);
-        }
-        else
-        {
-            auto u8str = ulib::Converter<AllocatorT>::Convert<EncodingT, ulib::Utf8>(p);
-            fmt::string_view name((char *)u8str.Data(), u8str.Size());
-
-            fmt::formatter<fmt::string_view> ft;
-            return ft.format(name, ctx);
-        }
+        return ulib::detail::FormatHandler<FormatContext, EncodingT>(p, ctx);
     }
 };
 
@@ -61,28 +109,15 @@ struct fmt::formatter<ulib::EncodedStringView<EncodingT>>
     template <typename FormatContext>
     auto format(const StringViewT &p, FormatContext &ctx)
     {
-        if constexpr (std::is_same_v<EncodingT, ulib::Utf8>)
-        {
-            fmt::string_view name((char *)p.Data(), p.Size());
-
-            fmt::formatter<fmt::string_view> ft;
-            return ft.format(name, ctx);
-        }
-        else
-        {
-            auto u8str = ulib::u8(p);
-            fmt::string_view name((char *)u8str.Data(), u8str.Size());
-
-            fmt::formatter<fmt::string_view> ft;
-            return ft.format(name, ctx);
-        }
+        return ulib::detail::FormatHandler(p, ctx);
     }
 };
 
 namespace ulib
 {
-    inline ulib::u8string vformat(fmt::basic_string_view<char> format_str,
-                           fmt::basic_format_args<fmt::buffer_context<fmt::type_identity_t<char>>> args)
+    /*
+       inline ulib::u8string vformat(fmt::basic_string_view<char> format_str,
+                                  fmt::basic_format_args<fmt::buffer_context<fmt::type_identity_t<char>>> args)
     {
         fmt::basic_memory_buffer<char> buffer;
         fmt::detail::vformat_to(buffer, format_str, args);
@@ -104,40 +139,98 @@ namespace ulib
         fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
         return ulib::vformat(vv, fmt::make_format_args(args...));
     }
+    */
 
-    template <class EncodingT, class AllocatorT = ulib::DefaultAllocator>
-    inline ulib::EncodedString<EncodingT, AllocatorT> vformat(fmt::basic_string_view<char> format_str,
-                                                       fmt::basic_format_args<fmt::buffer_context<fmt::type_identity_t<char>>> args)
+    /*
+#ifdef __cpp_char8_t
+template <class EncodingT, class AllocatorT = ulib::DefaultAllocator, typename... T>
+inline ulib::EncodedString<EncodingT, AllocatorT> format(ulib::string_view fmt, T &&...args)
+{
+    fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
+    return ulib::vformat<EncodingT, AllocatorT>(vv, fmt::make_format_args<fmt::buffer_context<char>>(args...));
+}
+#endif
+
+template <class EncodingT, class AllocatorT = ulib::DefaultAllocator, typename... T>
+inline ulib::EncodedString<EncodingT, AllocatorT> format(ulib::u8string_view fmt, T &&...args)
+{
+    fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
+    return ulib::vformat<EncodingT, AllocatorT>(vv, fmt::make_format_args<fmt::buffer_context<char>>(args...));
+}
+*/
+
+    template <class EncodingT, class AllocatorT = DefaultAllocator>
+    inline EncodedString<EncodingT, AllocatorT> vformat(fmt::basic_string_view<char> format_str,
+                                                        fmt::basic_format_args<fmt::buffer_context<fmt::type_identity_t<char>>> args)
     {
-        ulib::StandardAllocator<char, AllocatorT> al{};
-        fmt::basic_memory_buffer<char, 0x1000, ulib::StandardAllocator<char, AllocatorT>> buffer(al);
+        StandardAllocator<char, detail::FormatAllocatorT> al{};
+        fmt::basic_memory_buffer<char, 0x1000, ulib::StandardAllocator<char, detail::FormatAllocatorT>> buffer(al);
 
         fmt::detail::vformat_to(buffer, format_str, args);
-        ulib::EncodedString<ulib::Utf8, AllocatorT> temp((char8 *)buffer.data(), buffer.size());
 
-        if constexpr (std::is_same_v<EncodingT, ulib::Utf8>)
+        if constexpr (detail::IsEncodingFmtCompatible<EncodingT>)
         {
-            return temp;
+            return ulib::EncodedString<EncodingT, AllocatorT>((typename EncodingT::CharT *)buffer.data(), buffer.size());
         }
         else
         {
-            return ulib::Converter<AllocatorT>::Convert<ulib::Utf8, EncodingT>(temp);
+            return ulib::Convert<EncodingT, AllocatorT>(EncodedStringView<Utf8>{(char8 *)buffer.data(), buffer.size()});
         }
     }
 
-#ifdef __cpp_char8_t
-    template <class EncodingT, class AllocatorT = ulib::DefaultAllocator, typename... T>
-    inline ulib::EncodedString<EncodingT, AllocatorT> format(ulib::string_view fmt, T &&...args)
+    template <class UOutputEncodingT = void, class UOutputAllocatorT = void, class EncodingT, class AllocatorT,
+              class OutputEncodingT = detail::SelectTypeT<UOutputEncodingT, EncodingT>,
+              class OutputAllocatorT = detail::SelectTypeT<UOutputAllocatorT, AllocatorT>, typename... T>
+    inline ulib::EncodedString<OutputEncodingT, OutputAllocatorT> format(const ulib::EncodedString<EncodingT, AllocatorT> &fmt, T &&...args)
     {
-        fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
-        return ulib::vformat<EncodingT, AllocatorT>(vv, fmt::make_format_args<fmt::buffer_context<char>>(args...));
-    }
-#endif
+        if constexpr (detail::IsEncodingFmtCompatible<EncodingT>)
+        {
+            fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
+            return ulib::vformat<OutputEncodingT, OutputAllocatorT>(vv, fmt::make_format_args(args...));
+        }
+        else
+        {
+            auto u8fmt = Convert<Utf8, detail::FormatAllocatorT>(fmt);
 
-    template <class EncodingT, class AllocatorT = ulib::DefaultAllocator, typename... T>
-    inline ulib::EncodedString<EncodingT, AllocatorT> format(ulib::u8string_view fmt, T &&...args)
+            fmt::basic_string_view<char> vv((char *)u8fmt.data(), u8fmt.size());
+            return ulib::vformat<OutputEncodingT, OutputAllocatorT>(vv, fmt::make_format_args(args...));
+        }
+    }
+
+    template <class UOutputEncodingT = void, class OutputAllocatorT = DefaultAllocator, class EncodingT,
+              class OutputEncodingT = detail::SelectTypeT<UOutputEncodingT, EncodingT>, typename... T>
+    inline ulib::EncodedString<OutputEncodingT, OutputAllocatorT> format(ulib::EncodedStringView<EncodingT> fmt, T &&...args)
     {
-        fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
-        return ulib::vformat<EncodingT, AllocatorT>(vv, fmt::make_format_args<fmt::buffer_context<char>>(args...));
+        if constexpr (detail::IsEncodingFmtCompatible<EncodingT>)
+        {
+            fmt::basic_string_view<char> vv((char *)fmt.data(), fmt.size());
+            return ulib::vformat<OutputEncodingT, OutputAllocatorT>(vv, fmt::make_format_args(args...));
+        }
+        else
+        {
+            auto u8fmt = Convert<Utf8, detail::FormatAllocatorT>(fmt);
+
+            fmt::basic_string_view<char> vv((char *)u8fmt.data(), u8fmt.size());
+            return ulib::vformat<OutputEncodingT, OutputAllocatorT>(vv, fmt::make_format_args(args...));
+        }
+    }
+
+    template <class UOutputEncodingT = void, class OutputAllocatorT = DefaultAllocator, class CharT, class EncodingT = LiteralEncodingT<CharT>,
+              class OutputEncodingT = detail::SelectTypeT<UOutputEncodingT, EncodingT>,
+              std::enable_if_t<!std::is_same_v<EncodingT, void>, bool> = true, typename... T>
+    inline EncodedString<OutputEncodingT, OutputAllocatorT> format(const CharT *fmt, T &&...args)
+    {
+        if constexpr (detail::IsEncodingFmtCompatible<EncodingT>)
+        {
+            fmt::basic_string_view<char> vv((char *)fmt, CStringLengthHack(fmt));
+            return ulib::vformat<OutputEncodingT, OutputAllocatorT>(vv, fmt::make_format_args(args...));
+        }
+        else
+        {
+            auto u8fmt = Convert<Utf8, detail::FormatAllocatorT>(fmt);
+
+            fmt::basic_string_view<char> vv((char *)u8fmt.data(), u8fmt.size());
+            return ulib::vformat<OutputEncodingT, OutputAllocatorT>(vv, fmt::make_format_args(args...));
+        }
     }
 }
