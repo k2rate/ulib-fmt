9aafc40822ad2027485deef0cb5585f0bf3ae45a
k2re8 <dns.1983n@gmail.com>
2023-08-06 21:29:37 +0300

    [tests] placeholder tests for check github CI

--- tests/src/containers.cpp
+++ tests/src/containers.cpp
@@ -1,13 +1,28 @@
 #include <gtest/gtest.h>
 
-#include <ulib/format.h>
-#include <ulib/fmt/list.h>
-#include <ulib/string.h>
+// #include <ulib/format.h>
+// #include <ulib/fmt/list.h>
+// #include <ulib/string.h>
 
-TEST(Format, List)
-{
-    ulib::list<ulib::u32string> list = {U"hello", U"bye", U"fullplak"};
-    auto result = ulib::format("{}", list);
+// TEST(Format, List)
+// {
+//     ulib::list<ulib::u32string> list = {U"hello", U"bye", U"fullplak"};
+//     auto result = ulib::format("{}", list);
  
-    ASSERT_EQ(result, R"(["hello", "bye", "fullplak"])");
+//     ASSERT_EQ(result, R"(["hello", "bye", "fullplak"])");
+// }
+
+TEST(GithubCICheck, Check0)
+{
+
+}
+
+TEST(GithubCICheck, Check1)
+{
+    
+}
+
+TEST(GithubCICheck, Check2)
+{
+    
 }
