f646576e0835fae605062657ad5bb654c60bba68
k2re8 <dns.1983n@gmail.com>
2023-08-06 21:39:59 +0300

    [tests] check github CI

--- tests/src/containers.cpp
+++ tests/src/containers.cpp
@@ -3,19 +3,17 @@
 // #include <ulib/format.h>
 // #include <ulib/fmt/list.h>
 #include <ulib/string.h>
+#include <ulib/format.h>
 
 // TEST(Format, List)
 // {
 //     ulib::list<ulib::u32string> list = {U"hello", U"bye", U"fullplak"};
 //     auto result = ulib::format("{}", list);
- 
+
 //     ASSERT_EQ(result, R"(["hello", "bye", "fullplak"])");
 // }
 
-TEST(GithubCICheck, Check0)
-{
-    ASSERT_EQ(1, 1);
-}
+TEST(GithubCICheck, Check0) { ASSERT_EQ(1, 1); }
 
 TEST(GithubCICheck, Check1)
 {
@@ -27,4 +25,12 @@ TEST(GithubCICheck, Check2)
 {
     ulib::string str;
     ASSERT_EQ(3, 3);
+}
+
+TEST(GithubCICheck, Check3)
+{
+    ulib::list<ulib::u32string> list = {U"hello", U"bye", U"fullplak"};
+    auto result = ulib::format("{}", list);
+
+    ASSERT_EQ(result, R"(["hello", "bye", "fullplak"])");
 }
