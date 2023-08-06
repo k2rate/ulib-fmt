e6e23c1c6831dceeaae3e10c287100211bee5f5f
k2re8 <dns.1983n@gmail.com>
2023-08-06 21:35:54 +0300

    [tests] check github CI

--- tests/src/containers.cpp
+++ tests/src/containers.cpp
@@ -14,15 +14,15 @@
 
 TEST(GithubCICheck, Check0)
 {
-
+    ASSERT_EQ(1, 1);
 }
 
 TEST(GithubCICheck, Check1)
 {
-    
+    ASSERT_EQ(2, 2);
 }
 
 TEST(GithubCICheck, Check2)
 {
-    
+    ASSERT_EQ(3, 3);
 }
